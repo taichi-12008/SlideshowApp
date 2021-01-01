4c206ac7ae627585ea2e42828a1ddf7a2d804b80
taichi.imaoka <taichi0305.12008@gmail.com>
2021-01-02 02:54:45 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by 今岡大智 on 2021/01/02.
+//  Copyright © 2021 taichi.imaoka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
